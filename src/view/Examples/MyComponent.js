d13f08e903d8fd7652cddc9d385bee56690634fe
Nguyen Minh Dang <minhdang26082001@gmail.com>
2022-07-26 22:09:03 +0700

    video #5

--- src/view/Examples/MyComponent.js
+++ src/view/Examples/MyComponent.js
@@ -1,7 +1,16 @@
 import React from "react";
 class MyComponent extends React.Component {
+  state = {
+    name: "dang",
+    age: 21,
+  };
   render() {
-    return <h1>hello form MyComponent</h1>;
+    return (
+      <>
+        <h1>hello form MyComponent my name is {this.state.name}</h1>
+        <div className="mydiv">i am {this.state.age} year old</div>
+      </>
+    );
   }
 }
 export default MyComponent;
