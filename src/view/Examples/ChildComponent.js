97c89a6d860a7efad2ae9995e208c6abcddb2974
Nguyen Minh Dang <minhdang26082001@gmail.com>
2022-07-28 21:39:15 +0700

    video #14

---
+++ src/view/Examples/ChildComponent.js
@@ -0,0 +1,44 @@
+import React from "react";
+class ChildComponent extends React.Component {
+  state = {
+    showList: false,
+  };
+  handleShow = () => {
+    // if (this.state.showList === false) {
+    //   this.setState({ showList: true });
+    // } else {
+    //   this.setState({ showList: false });
+    // }
+    this.setState({
+      showList: !this.state.showList,
+    });
+  };
+  render() {
+    const { jobs } = this.props;
+    return (
+      <>
+        {this.state.showList === false ? (
+          <div>
+            <button onClick={() => this.handleShow()}>Show</button>
+          </div>
+        ) : (
+          <>
+            <div>
+              {jobs.map((item, index) => {
+                return (
+                  <div className="div" key={item.id}>
+                    {item.content}-{item.salary}
+                  </div>
+                );
+              })}
+            </div>
+            <div>
+              <button onClick={() => this.handleShow()}>Hide</button>
+            </div>
+          </>
+        )}
+      </>
+    );
+  }
+}
+export default ChildComponent;
--- src/view/Examples/MyComponent.js
+++ src/view/Examples/MyComponent.js
@@ -1,14 +1,54 @@
 import React from "react";
+import ChildComponent from "./ChildComponent";
 class MyComponent extends React.Component {
   state = {
     name: "dang",
     age: 21,
+    jobs: [
+      { id: "job1", content: "Dev", salary: "500$" },
+      { id: "job2", content: "Tester", salary: "400$" },
+      { id: "job3", content: "Project manager", salary: "1500$" },
+    ],
+  };
+  handleChangeInput = (e) => {
+    this.setState({
+      name: e.target.value,
+    });
+  };
+  handleChangeAge = (e) => {
+    this.setState({
+      age: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
   };
   render() {
     return (
       <>
-        <h1>hello form MyComponent my name is {this.state.name}</h1>
-        <div className="mydiv">i am {this.state.age} year old</div>
+        <form>
+          <label>Name</label>
+          <br />
+          <input
+            type="text"
+            onChange={(e) => this.handleChangeInput(e)}
+            value={this.state.name}
+          ></input>
+          <br />
+          <label>Age</label>
+          <br />
+          <input
+            type="text"
+            onChange={(e) => this.handleChangeAge(e)}
+            value={this.state.age}
+          ></input>
+          <br />
+          <button type="submit" onClick={(e) => this.handleSubmit(e)}>
+            submit
+          </button>
+        </form>
+        <ChildComponent jobs={this.state.jobs} />
       </>
     );
   }
